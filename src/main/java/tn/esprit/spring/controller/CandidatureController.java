f1d3b65d4c1dbfaa9e20a6ac301e85331ffbffb6
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-12-24 12:06:34 +0100

    Canidature Controller

---
+++ src/main/java/tn/esprit/spring/controller/CandidatureController.java
@@ -0,0 +1,64 @@
+package tn.esprit.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import tn.esprit.spring.entity.Candidature;
+import tn.esprit.spring.entity.Offre;
+import tn.esprit.spring.service.ICandidatureService;
+import tn.esprit.spring.service.OffreService;
+
+@RestController
+@RequestMapping("/candidature")
+
+public class CandidatureController {
+	@Autowired
+	ICandidatureService candidatService;	
+	
+	
+	@PostMapping("/add-candidature")
+	public void addCandidature(@RequestBody Candidature f) {
+		candidatService.addCandidature(f);
+
+	}	
+	
+	
+	@DeleteMapping("/supprimer-candidature/{idCandidature}")
+	@ResponseBody
+	public void deleteCandidature(
+			@PathVariable("idCandidature") Long idCandidature) {
+		candidatService.deleteCandidature(idCandidature);
+
+	}
+	
+	
+	@GetMapping("/getAllCandidature")
+	public List<Candidature> getAllCandidature() {
+
+	
+
+		return candidatService.listAllCandidature();
+
+	}
+	
+	/*
+	@PutMapping("/{idCandidature}")
+	public void UpdateCandidature(@RequestBody Candidature f, @PathVariable("idCandidature") Long idCandidature)
+			 {
+		candidatService.updateCandidature(f, idCandidature);
+
+	}*/
+	
+	
+}
--- src/main/java/tn/esprit/spring/controller/OffreController.java
+++ src/main/java/tn/esprit/spring/controller/OffreController.java
@@ -1,5 +1,5 @@
 package tn.esprit.spring.controller;
-import java.util.Date;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,31 +13,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+
 import tn.esprit.spring.entity.Offre;
-import tn.esprit.spring.service.OffreService;
+import tn.esprit.spring.service.IOffreService;
+
 
 
 
 
 @RestController
 @RequestMapping("/offre")
-@Api(tags = "Gestion Offre")
+
 public class OffreController {
 	
 	@Autowired
-	OffreService offreService;	
+	IOffreService offreService;	
 	
-	@ApiOperation(value = "Ajouter Offre")
+	
 	@PostMapping("/add-offre")
 	public void addOffre(@RequestBody Offre f) {
 		offreService.addOffre(f);
 
 	}	
 	
-	@ApiOperation(value = "Supprimer Offre")
-	@DeleteMapping("/supprimer-offre")
+	
+	@DeleteMapping("/supprimer-offre/{idOffre}")
 	@ResponseBody
 	public void deleteOffre(
 			@PathVariable("idOffre") Long idOffre) {
@@ -45,9 +45,9 @@ public class OffreController {
 
 	}
 	
-	@ApiOperation(value = "Afficher Offre ")
-	@GetMapping("/GetOffre")
-	public List<Offre> getOffre() {
+	
+	@GetMapping("/getAllOffre")
+	public List<Offre> getAllOffre() {
 
 	
 
@@ -55,7 +55,7 @@ public class OffreController {
 
 	}
 	
-	@ApiOperation(value = "Modifier Offre")
+	
 	@PutMapping("/{idOffre}")
 	public void UpdateOffre(@RequestBody Offre f, @PathVariable("idOffre") Long idOffre)
 			 {
