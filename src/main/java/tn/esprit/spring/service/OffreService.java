105be0219f6718ce809770097330ce47daa1b6a6
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-12-24 11:53:04 +0100

    Update OffreService.java

--- src/main/java/tn/esprit/spring/service/OffreService.java
+++ src/main/java/tn/esprit/spring/service/OffreService.java
@@ -1,55 +1,39 @@
 package tn.esprit.spring.service;
-import java.util.ArrayList;
-import java.util.Collections;
+
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
+
 import org.springframework.stereotype.Service;
 
-import lombok.extern.slf4j.Slf4j;
+
 import tn.esprit.spring.entity.Offre;
-import tn.esprit.spring.entity.TypeOffre;
+
 import tn.esprit.spring.repository.OffreRepository;
 
 @Service
 public class OffreService implements IOffreService {
 	
-
-	
 		@Autowired
 		OffreRepository offreRepo;
 	
 
-	/////////////////////////Ajout Offre//////////////////
 		
 		@Override
-		public void addOffre(Offre offre) {
-			
+		public void addOffre(Offre offre) {		
 			offreRepo.save(offre);
-
 			 
 		}
 
-	///////////////////////Delete Offre//////////////////////
 		@Override
 		public void deleteOffre( Long idOffre) {
-
-			
-		
-			
 			offreRepo.deleteById(idOffre);
 			
 		}
@@ -60,13 +44,10 @@ public class OffreService implements IOffreService {
 			
 		}
 	
-		///////////////////////Update Offre//////////////////////
 		@Override
 		public void updateOffre(Offre offre, Long idOffre) {
 			Date currentSqlDate = new Date(System.currentTimeMillis());
-			Offre off = offreRepo.findById(idOffre).orElse(null);
-
-			
+			Offre off = offreRepo.findById(idOffre).orElse(null);	
 			off.setOffreId(idOffre);
 			off.setDatePublication(currentSqlDate);
 			off.setTitre(offre.getTitre());
@@ -74,12 +55,8 @@ public class OffreService implements IOffreService {
 			off.setDescription(offre.getDescription());
 			off.setAdresse(offre.getAdresse());
 			off.setSalaire(offre.getSalaire());
-			
-			
-			
 			offreRepo.save(off);
 			
-		
 		}
 
 		
